a71d1dc58c525d5463dc4b37f6c06e884c8759ec
Redzikesxd <tomekreda16@gmail.com>
2018-03-04 21:57:20 +0100

    Poczatek

---
+++ src/main/java/pl/orgella/OrgellaApplication.java
@@ -0,0 +1,12 @@
+package pl.orgella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgellaApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/orgella/orgella/OrgellaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.orgella.orgella;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrgellaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
